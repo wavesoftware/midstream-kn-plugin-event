f85bcc132d635022ef9abacbb848608a71cfe17a
Chris Suszyński <ksuszyns@redhat.com>
2021-11-20 02:22:26 +0100

    Upgrade deviate tool

--- openshift/lib.bash
+++ openshift/lib.bash
@@ -3,20 +3,40 @@
 set -Eeuo pipefail
 
 # go.get-tool will 'go get' any package $2 and install it to $1. Replaces, can
-# be set with $3.
+# be set with $GO_GETTOOL_REPLACES.
 function go.get-tool {
-  local tmp_dir replace
+  local tmp_dir
   if ! [ -f "${1}" ]; then
-    replace="${3:-}"
     tmp_dir="$(mktemp -d)"
     # shellcheck disable=SC2064
     trap "rm -rf ${tmp_dir}" EXIT
     cd "$tmp_dir"
     go mod init tmp
-    if [ -n "${replace:-}" ]; then
-      go mod edit -replace "${replace}"
+    if [ -n "${GO_GETTOOL_REPLACES:-}" ]; then
+      go mod edit -replace "${GO_GETTOOL_REPLACES}"
     fi
     echo "Downloading ${2}"
     GOBIN="$(dirname "${1}")" go get "${2}"
   fi
+  echo "Using ${2} in ${1}"
+}
+
+# go.run-tool will 'go get' package $1 and install it to temp path. Replaces, 
+# can be set with $GO_GETTOOL_REPLACES. You could pass arguments to the tool by
+# setting $2 and up.
+function go.run-tool {
+  local path_id systmp_dir bin_dir package package_id bin
+  package="${1:?'Pass a GO executable package ref as arg[1]'}"
+  shift
+  path_id="$(echo "${BASH_SOURCE[0]:-$0}" | sha1sum - | awk '{print $1}')"
+  package_id="$(echo "$package" | sha1sum - | awk '{print $1}')"
+  systmp_dir="$(dirname "$(mktemp -d -u)")"
+  bin_dir="${systmp_dir}/go-run-tool/${path_id}/${package_id}"
+  mkdir -p "$bin_dir"
+  bin="${package%@*}"
+  bin="${bin_dir}/${bin##*/}"
+
+  go.get-tool "$bin" "$package"
+
+  "$bin" "$@"
 }
--- openshift/release/update-to-head.sh
+++ openshift/release/update-to-head.sh
@@ -2,13 +2,10 @@
 
 set -Eeuo pipefail
 
-project_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../..")"
+project_dir="$(realpath "$(dirname "${BASH_SOURCE[0]:-$0}")/../..")"
 # shellcheck source=../lib.bash
 source "${project_dir}/openshift/lib.bash"
-tools="$(mktemp -d)"
 
-go.get-tool "${tools}/deviate" \
-  github.com/cardil/deviate/cmd/deviate@v0 \
-  github.com/shurcooL/graphql=github.com/cli/shurcooL-graphql@v0.0.0-20200707151639-0f7232a2bf7e
-
-"${tools}/deviate" sync --config "${project_dir}/.deviate.yaml"
+go.run-tool \
+  github.com/cardil/deviate/cmd/deviate@v0.1.2 \
+  sync --config "${project_dir}/.deviate.yaml"
