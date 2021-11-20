35c791ed8c707a5d15d95d7b4fcc1f1f038da6f4
Chris Suszyński <ksuszyns@redhat.com>
2021-11-20 01:53:40 +0100

    Install tools in tmp directory

--- openshift/release/update-to-head.sh
+++ openshift/release/update-to-head.sh
@@ -5,10 +5,10 @@ set -Eeuo pipefail
 project_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../..")"
 # shellcheck source=../lib.bash
 source "${project_dir}/openshift/lib.bash"
+tools="$(mktemp -d)"
 
-go.get-tool "${project_dir}/build/tools/deviate" \
+go.get-tool "${tools}/deviate" \
   github.com/cardil/deviate/cmd/deviate@v0 \
   github.com/shurcooL/graphql=github.com/cli/shurcooL-graphql@v0.0.0-20200707151639-0f7232a2bf7e
 
-"${project_dir}/build/tools/deviate" sync \
-  --config "${project_dir}/.deviate.yaml"
+"${tools}/deviate" sync --config "${project_dir}/.deviate.yaml"
