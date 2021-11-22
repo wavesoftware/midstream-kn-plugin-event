917cd37f62edb943df0647ce9277a7ba6e89201c
Chris Suszyński <ksuszyns@redhat.com>
2021-11-22 15:47:02 +0100

    Switch to Golang native package management

--- openshift/release/update-to-head.sh
+++ openshift/release/update-to-head.sh
@@ -3,9 +3,9 @@
 set -Eeuo pipefail
 
 project_dir="$(realpath "$(dirname "${BASH_SOURCE[0]:-$0}")/../..")"
-# shellcheck source=../lib.bash
-source "${project_dir}/openshift/lib.bash"
 
-go.run-tool \
-  github.com/cardil/deviate/cmd/deviate@v0.1.2 \
+cd "$project_dir/openshift"
+
+exec go run \
+  github.com/cardil/deviate/cmd/deviate \
   sync --config "${project_dir}/.deviate.yaml"
